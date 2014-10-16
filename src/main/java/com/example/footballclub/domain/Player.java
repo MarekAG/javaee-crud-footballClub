db65cc6c3ae0203ba90edf8c0fa1cc148244b309
Marek Strzała <marekwstrzala@gmail.com>
2014-10-16 17:10:37 +0200

    fix deleting test

--- src/main/java/com/example/footballclub/domain/Player.java
+++ src/main/java/com/example/footballclub/domain/Player.java
@@ -1,34 +1,38 @@
 package com.example.footballclub.domain;
 
 public class Player {
-	
+
 	private long id;
-	
+
 	private String name;
 	private String lastName;
 	private String position;
 	private int age;
-	
+
 	public Player() {
 	}
-	
+
 	public Player(String name, String lastName, String position, int age) {
 		super();
 		this.name = name;
 		this.lastName = lastName;
 		this.position = position;
 		this.age = age;
-		
+
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
--- src/test/java/com/example/footballclub/service/PlayerManagerTest.java
+++ src/test/java/com/example/footballclub/service/PlayerManagerTest.java
@@ -10,121 +10,119 @@ import com.example.footballclub.domain.Player;
 import com.example.footballclub.service.PlayerManager;
 
 public class PlayerManagerTest {
-	
-	
+
 	PlayerManager playerManager = new PlayerManager();
 	private final static String NAME_1 = "Grzegorz";
 	private final static String LASTNAME_1 = "Niciński";
 	private final static String POSITION_1 = "Striker";
 	private final static int AGE_1 = 35;
-	
+
 	private final static String NAME_2 = "Krzysztof";
 	private final static String LASTNAME_2 = "Sobieraj";
-	private final static String POSITION_2 = "Defender";	
+	private final static String POSITION_2 = "Defender";
 	private final static int AGE_2 = 31;
-	
-	
+
 	@Test
-	public void checkConnection(){
+	public void checkConnection() {
 		assertNotNull(playerManager.getConnection());
 	}
-	
+
 	@Test
-	public void checkAdding(){
-		
+	public void checkAdding() {
+
 		Player player = new Player(NAME_1, LASTNAME_1, POSITION_1, AGE_1);
-		
+
 		playerManager.clearPlayers();
-		assertEquals(1,playerManager.addPlayer(player));
-		
+		assertEquals(1, playerManager.addPlayer(player));
+
 		List<Player> players = playerManager.getAllPlayers();
 		Player personRetrieved = players.get(0);
-		
+
 		assertEquals(NAME_1, personRetrieved.getName());
 		assertEquals(LASTNAME_1, personRetrieved.getLastName());
 		assertEquals(POSITION_1, personRetrieved.getPosition());
 		assertEquals(AGE_1, personRetrieved.getAge());
-		
+
 	}
-	
+
 	@Test
 	public void checkDeleting() {
 		playerManager.clearPlayers();
 		Player player = new Player(NAME_1, LASTNAME_1, POSITION_1, AGE_1);
-		assertEquals(1,playerManager.addPlayer(player));
-		
-		
+		assertEquals(1, playerManager.addPlayer(player));
+
 		List<Player> players = playerManager.getAllPlayers();
 		Player pr = players.get(0);
-		
-		assertSame(1,playerManager.deletePlayer(pr));	
+
+		assertSame(1, playerManager.deletePlayer(pr));
+
+		assertNull(playerManager.getPlayer(pr.getId()));
 	}
-	
+
 	@Test
 	public void checkAllDeleting() {
 		playerManager.clearPlayers();
 		Player player = new Player(NAME_1, LASTNAME_1, POSITION_1, AGE_1);
-		assertEquals(1,playerManager.addPlayer(player));
-		assertEquals(1,playerManager.addPlayer(player));
-		assertEquals(1,playerManager.addPlayer(player));
-		
+		assertEquals(1, playerManager.addPlayer(player));
+		assertEquals(1, playerManager.addPlayer(player));
+		assertEquals(1, playerManager.addPlayer(player));
+
 		Player player2 = new Player(NAME_2, LASTNAME_2, POSITION_2, AGE_2);
-		assertEquals(1,playerManager.addPlayer(player2));
-		assertEquals(1,playerManager.addPlayer(player2));
-		assertEquals(1,playerManager.addPlayer(player2));
-		
+		assertEquals(1, playerManager.addPlayer(player2));
+		assertEquals(1, playerManager.addPlayer(player2));
+		assertEquals(1, playerManager.addPlayer(player2));
+
 		List<Player> players = playerManager.getAllPlayers();
-		
+
 		assertSame(6, players.size());
-		
+
 		playerManager.clearPlayers();
-		
+
 		players = playerManager.getAllPlayers();
-		
+
 		assertSame(0, players.size());
 	}
-	
+
 	@Test
 	public void checkUpdate() {
-				
+
 		playerManager.clearPlayers();
-		
+
 		Player player = new Player(NAME_1, LASTNAME_1, POSITION_1, AGE_1);
-		
-		assertEquals(1,playerManager.addPlayer(player));
-		
+
+		assertEquals(1, playerManager.addPlayer(player));
+
 		List<Player> players = playerManager.getAllPlayers();
 		Player pr = players.get(0);
-		
+
 		assertEquals(POSITION_1, pr.getPosition());
-		
+
 		assertEquals(1, playerManager.updatePlayer(pr, POSITION_2));
-		
+
 		players = playerManager.getAllPlayers();
 		Player pr2 = players.get(0);
-		
+
 		assertEquals(POSITION_2, pr2.getPosition());
 
-		
 	}
-	
+
 	@Test
 	public void checkSelectOne() {
 		playerManager.clearPlayers();
-		
+
 		Player player = new Player(NAME_1, LASTNAME_1, POSITION_1, AGE_1);
-		
-		assertEquals(1,playerManager.addPlayer(player));
-		
+
+		assertEquals(1, playerManager.addPlayer(player));
+
 		List<Player> players = playerManager.getAllPlayers();
 		Player pr = players.get(0);
-		
+
 		long id = pr.getId();
-		
+
 		assertNotNull(playerManager.getPlayer(id));
-		
+
 		Player personRetrieved = playerManager.getPlayer(id);
-		
+
 		assertEquals(NAME_1, personRetrieved.getName());
 		assertEquals(LASTNAME_1, personRetrieved.getLastName());
 		assertEquals(POSITION_1, personRetrieved.getPosition());
